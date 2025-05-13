674ae604c3bf3bf7822436e8346fccd05ecf67d3
Gustavo Fernandes <gustavofernandes330@gmail.com>
2025-05-13 17:48:56 -0300

    Correcão do Swagger
    Atualizacão de alguns packages no POM | Criacão do application.properties | Configuracão geral do Swagger e anotacoes na classe de controller

--- src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
+++ src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -1,15 +1,10 @@
 package one.digitalinnovation.gof.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import one.digitalinnovation.gof.model.Cliente;
 import one.digitalinnovation.gof.service.ClienteService;
@@ -18,38 +13,42 @@ import one.digitalinnovation.gof.service.ClienteService;
  * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
  * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
  * interface simples e coesa (API REST).
- * 
- * @author falvojr
  */
+@Api(value = "Clientes", tags = {"Clientes"})
 @RestController
-@RequestMapping("clientes")
+@RequestMapping("/clientes")
 public class ClienteRestController {
 
 	@Autowired
 	private ClienteService clienteService;
 
+	@ApiOperation(value = "Retorna todos os clientes")
 	@GetMapping
 	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
 		return ResponseEntity.ok(clienteService.buscarTodos());
 	}
 
+	@ApiOperation(value = "Busca um cliente por ID")
 	@GetMapping("/{id}")
 	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
 		return ResponseEntity.ok(clienteService.buscarPorId(id));
 	}
 
+	@ApiOperation(value = "Insere um novo cliente")
 	@PostMapping
 	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
 		clienteService.inserir(cliente);
 		return ResponseEntity.ok(cliente);
 	}
 
+	@ApiOperation(value = "Atualiza um cliente existente")
 	@PutMapping("/{id}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
 		clienteService.atualizar(id, cliente);
 		return ResponseEntity.ok(cliente);
 	}
 
+	@ApiOperation(value = "Remove um cliente pelo ID")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deletar(@PathVariable Long id) {
 		clienteService.deletar(id);
